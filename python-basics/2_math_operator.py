54e1c9d7e80236a6359f3346829e7c9051e51674
uthircloudnative <uthircloudnative@gmail.com>
2024-12-12 17:46:59 -0600

    First Commit

---
+++ python-basics/2_math_operator.py
@@ -0,0 +1,5 @@
+print(2+8) # Addition
+print(10-8) # Subtraction
+print(2*8) # Multiplication
+print(10/7) # Division
+print(10**4) # Exponentiation
---
+++ python-basics/3_builtin_function.py
@@ -0,0 +1,7 @@
+# Max function
+print(max(19,-7,28,78))
+print(max(19,-7,28,78,(10*10)))
+
+# Min function
+
+print(min(19,-7,28,78))
